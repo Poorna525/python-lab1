46184390f882437e94c6c5594c3d0ae4fbbf7526
Poorna525 <70189778+Poorna525@users.noreply.github.com>
2020-09-06 19:28:03 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,21 @@
+#arithmtic operations
+
+#input
+a=int(input("enter number1 : "))
+b=int(input("enter number2 : "))
+
+#addition
+print("sum is :",a+b)
+
+#subtraction
+print("difference is :",a-b)
+
+#multiplication
+print("product is :",a*b)
+
+#division
+print("quotient is :",a//b)#floor division
+print("div-result is :" ,a/b)#float values as result
+
+#remainder
+print("remainder is :" ,a%b)
---
+++ swapping.py
@@ -0,0 +1,7 @@
+#swapping of numbers
+
+#input
+a=int(input("enter num1: "))
+b=int(input("enter num2: "))
+a,b=b,a
+print(a,b)
